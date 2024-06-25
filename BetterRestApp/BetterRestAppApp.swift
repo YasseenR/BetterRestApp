1cc50307df30c9336af0691e748ce99d88517be3
YasseenR <yasseenrouni@gmail.com>
2024-06-25 15:50:28 -0400

    Initial Commit

---
+++ BetterRestApp/BetterRestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestAppApp.swift
+//  BetterRestApp
+//
+//  Created by Yasseen Rouni on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
