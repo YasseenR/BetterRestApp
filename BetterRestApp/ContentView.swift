fa2af0689841fe53a9ac328af7345a2fd0474833
YasseenR <yasseenrouni@gmail.com>
2024-07-01 12:25:04 -0400

    Updated date function. Created new view.

--- BetterRestApp/ContentView.swift
+++ BetterRestApp/ContentView.swift
@@ -12,13 +12,18 @@ struct ContentView: View {
     
     
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
+        Text(Date.now.formatted(date: .long, time:.shortened))
     }
     
     func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
